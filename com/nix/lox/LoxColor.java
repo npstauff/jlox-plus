dab5af1ffd1a589fd923206801e44c261c94b975
NixStudio <groovetablet@gmail.com>
2023-06-26 16:18:54 -0400

    color functions

--- com/nix/lox/LoxColor.java
+++ com/nix/lox/LoxColor.java
@@ -18,6 +18,12 @@ public class LoxColor extends LoxNative{
   private Map<String, LoxFunction> defineFunctions(Environment environment) {
     Map<String, LoxFunction> methods = new HashMap<>();
     methods.put("red", red(environment));
+    methods.put("green", green(environment));
+    methods.put("blue", blue(environment));
+    methods.put("black", black(environment));
+    methods.put("white", white(environment));
+    methods.put("set", set(environment));
+    methods.put("setAlpha", setAlpha(environment));
     return methods;
   }
 
@@ -44,6 +50,135 @@ public class LoxColor extends LoxNative{
       
     }, environment, false, true);
   }
+
+  private LoxFunction green(Environment environment) {
+    return new LoxFunction(new LoxCallable() {
+
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        Color color = Color.green;
+        LoxInstance instance = new LoxInstance(new LoxColor(environment, interpreter, type), interpreter);
+        instance.set("r", (double)color.getRed());
+        instance.set("g", (double)color.getGreen());
+        instance.set("b", (double)color.getBlue());
+        instance.set("a", (double)color.getAlpha());
+        return instance;
+      }
+
+      
+      
+    }, environment, false, true);
+
+    
+  }
+
+  private LoxFunction blue(Environment environment) {
+    return new LoxFunction(new LoxCallable() {
+
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        Color color = Color.blue;
+        LoxInstance instance = new LoxInstance(new LoxColor(environment, interpreter, type), interpreter);
+        instance.set("r", (double)color.getRed());
+        instance.set("g", (double)color.getGreen());
+        instance.set("b", (double)color.getBlue());
+        instance.set("a", (double)color.getAlpha());
+        return instance;
+      }
+      
+    }, environment, false, true);
+  }
+
+  private LoxFunction black(Environment environment) {
+    return new LoxFunction(new LoxCallable() {
+
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        Color color = Color.black;
+        LoxInstance instance = new LoxInstance(new LoxColor(environment, interpreter, type), interpreter);
+        instance.set("r", (double)color.getRed());
+        instance.set("g", (double)color.getGreen());
+        instance.set("b", (double)color.getBlue());
+        instance.set("a", (double)color.getAlpha());
+        return instance;
+      }
+      
+    }, environment, false, true);
+  }
+
+  private LoxFunction white(Environment environment) {
+    return new LoxFunction(new LoxCallable() {
+
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        Color color = Color.white;
+        LoxInstance instance = new LoxInstance(new LoxColor(environment, interpreter, type), interpreter);
+        instance.set("r", (double)color.getRed());
+        instance.set("g", (double)color.getGreen());
+        instance.set("b", (double)color.getBlue());
+        instance.set("a", (double)color.getAlpha());
+        return instance;
+      }
+      
+    }, environment, false, true);
+  }
+
+  private LoxFunction set(Environment environment) {
+    return new LoxFunction(new LoxCallable() {
+
+      @Override
+      public int arity() {
+        return 3;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        set("r", (double)arguments.get(0));
+        set("g", (double)arguments.get(1));
+        set("b", (double)arguments.get(2));
+        return null;
+      }
+      
+    }, environment, false, false);
+  }
+
+  private LoxFunction setAlpha(Environment environment) {
+    return new LoxFunction(new LoxCallable() {
+
+      @Override
+      public int arity() {
+        return 1;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        set("a", (double)arguments.get(0));
+        return null;
+      }
+      
+    }, environment, false, false);
+  }
+  
 
 @Override
   public void defineFields() {
